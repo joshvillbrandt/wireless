a54ff259cf4e334470741537a6a1ec3570e4afe2
Josh Villbrandt <josh@javconcepts.com>
2014-11-22 16:57:17 -0800

    Add super simple test

---
+++ tests/TestWireless.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import unittest
+from wireless import Wireless
+
+
+class TestGoPro(unittest.TestCase):
+    def setUp(self):
+        # TODO
+        pass
+
+    def test_import(self):
+        # if this module loads, then the import worked...
+        pass
