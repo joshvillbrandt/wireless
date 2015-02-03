55a611d0075cfa01644b7389306c9540a510012d
anatoly techtonik <techtonik@gmail.com>
2015-02-03 21:12:44 +0300

    Fix flake8 test

--- wireless/__main__.py
+++ wireless/__main__.py
@@ -1,51 +1,51 @@
-import os
 import sys
 
 import Wireless
 
-# Example Vista output for "netsh wlan show profiles"
 
-"""
-
-Profiles on interface Wireless Network Connection:
-
-Group Policy Profiles (read only)
----------------------------------
-    <None>
-
-User Profiles
--------------
-    All User Profile     : cashvault
-    All User Profile     : IBM-guest
-    All User Profile     : Guakamole
-
-"""
 def get_win_profiles():
     output = Wireless.cmd("netsh wlan show profiles")
+    # Example Vista output for "netsh wlan show profiles"
+    """
+
+    Profiles on interface Wireless Network Connection:
+
+    Group Policy Profiles (read only)
+    ---------------------------------
+        <None>
+
+    User Profiles
+    -------------
+        All User Profile     : cashvault
+        All User Profile     : IBM-guest
+        All User Profile     : Guakamole
+
+    """
     names = []
     for line in output.splitlines():
         if "All User Profile" in line:
             names.append(line.split(":")[-1].strip())
     return names
 
-# Example Vista output for "netsh wlan show profile <name>"
 
-"""
-...
-Connectivity settings
----------------------
-    Number of SSIDs        : 1
-    SSID name              : "cashvault"
-    Network type           : Infrastructure
-    Radio type             : [ Any Radio Type ]
-    Vendor extension       : Not present
-...
-"""
 def get_win_profile_ssid(profile):
     output = Wireless.cmd("netsh wlan show profile \"%s\"" % profile)
+    # Example Vista output for "netsh wlan show profile <name>"
+    """
+    ...
+    Connectivity settings
+    ---------------------
+        Number of SSIDs        : 1
+        SSID name              : "cashvault"
+        Network type           : Infrastructure
+        Radio type             : [ Any Radio Type ]
+        Vendor extension       : Not present
+    ...
+    """
     for line in output.splitlines():
         if "SSID name" in line:
             return line.split(":")[-1].strip(' "')
+
 
 def win_connect(ssid):
     print("[*] Get WiFi profiles")
